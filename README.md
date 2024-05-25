f06f97ca507238abbc3eb359d19cb3da49e3054a
abdelmaged <abdelmaged.khalifa@gmail.com>
2024-05-25 03:17:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# process-matcher
+process-matcher filters running processes by name, and get processId to be used in attach debug configuration.
+
+## Features
+Filters processes by:
+* Program name
+* Arugments to program
+
+## Extension Settings
+The extension creates command `process-matcher.match`, that takes the following options:
+* `program`: name of the program to match
+* `args`: arguments of the program to match
+
+## Notes
+* If number of matched processes is one, then it will automatically pick its PID.
+* If number of matched processes is more than one, then it will display a process picker with all matched processes.
+
+## Example
+launch.json
+```
+{
+  "version": "0.2.0",
+  "configurations": [
+    {
+      "name": "(gdb) attach myApp",
+      "type": "cppdbg",
+      "request": "attach",
+      "program": "${workspaceFolder}/myApp",
+      "processId": "${input:myAppMatcher}"
+      "MIMode": "gdb"
+    }
+  ],
+  "inputs": [
+    {
+      "id": "myAppMatcher",
+      "type": "command",
+      "command": "process-matcher.match",
+      "args": { "program": "myApp" }
+    }
+  ]
+}
+```
+
+## Demo
+1 - Matched multiple processes
+![](./demos/match-multi.gif)
+
+2 - Matched single process
+![](./demos/match-one.gif)
---
+++ src/extension.ts
@@ -0,0 +1,69 @@
+import * as vscode from 'vscode';
+const ps = require('ps-node');
+
+interface MatchOptions {
+  program: string;
+  args: string[];
+}
+
+interface ProcessInfo {
+  pid: number;
+  command: string;
+  arguments: string[];
+}
+
+function getProcessInfo(options: MatchOptions): Promise<ProcessInfo[] | null> {
+  return new Promise(resolve => {
+    ps.lookup({
+      command: options.program,
+      arguments: options.args
+    }, function (err: any, results: any[]) {
+      if (err) {
+        resolve(null);
+      }
+      resolve(results);
+    });
+  });
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand('process-matcher.match', async (options) => {
+    const pss = await getProcessInfo(options);
+
+    if (!pss || pss.length === 0) {
+      vscode.window.showWarningMessage(`No process matched ${JSON.stringify(options)}`);
+      return null;
+    }
+
+    if (pss.length === 1) {
+      vscode.window.showInformationMessage(`Matched process with pid '${pss[0].pid}'. Command: '${pss[0].command} ${pss[0].arguments.join(' ')}'`);
+      return pss[0].pid;
+    }
+
+    const quickPickItems: vscode.QuickPickItem[] = pss.map((ps) => {
+      const qp_item: vscode.QuickPickItem = {
+        label: ps.command,
+        detail: ps.pid.toString()
+      };
+      if (ps.arguments) {
+        qp_item.description = ps.arguments.join(' ');
+      }
+      return qp_item;
+    });
+
+    const pickedItem = await vscode.window.showQuickPick(quickPickItems, {
+      matchOnDescription: true,
+      matchOnDetail: true
+    });
+
+    if (pickedItem) {
+      return pickedItem.detail;
+    }
+
+    return null;
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
