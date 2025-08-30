2c0eb12b14067ca13a8344de3a15b18615fa9065
prata <609412062@qq.com>
2025-08-30 11:55:45 +0800

    Replace ps-node with find-process; wmic dependency breaks on modern Windows.

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-const ps = require('ps-node');
+const find = require('find-process');
 
 interface MatchOptions {
   program: string;
@@ -12,18 +12,29 @@ interface ProcessInfo {
   arguments: string[];
 }
 
-function getProcessInfo(options: MatchOptions): Promise<ProcessInfo[] | null> {
-  return new Promise(resolve => {
-    ps.lookup({
-      command: options.program,
-      arguments: options.args
-    }, function (err: any, results: any[]) {
-      if (err) {
-        resolve(null);
-      }
-      resolve(results);
-    });
-  });
+async function getProcessInfo(options: MatchOptions): Promise<ProcessInfo[]> {
+  try {
+    const processes = await find('name', options.program);
+    
+    let filteredProcesses = processes;
+    
+    // Filter by args if provided
+    if (options.args && options.args.length > 0) {
+      filteredProcesses = processes.filter((proc: any) => {
+        const cmd = proc.cmd || '';
+        return options.args.every(arg => cmd.includes(arg));
+      });
+    }
+    
+    // Convert to ProcessInfo format
+    return filteredProcesses.map((proc: any) => ({
+      pid: String(proc.pid),
+      command: proc.name,
+      arguments: proc.cmd ? proc.cmd.split(' ').slice(1) : []
+    }));
+  } catch (err) {
+    return [];
+  }
 }
 
 export function activate(context: vscode.ExtensionContext) {
